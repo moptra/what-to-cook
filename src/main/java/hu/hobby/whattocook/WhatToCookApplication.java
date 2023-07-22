cd774ec06e19ca3244abc75916a6276ac0794eed
moptra <117033540+moptra@users.noreply.github.com>
2023-07-22 13:00:06 +0200

    Initial commit

---
+++ src/main/java/hu/hobby/whattocook/WhatToCookApplication.java
@@ -0,0 +1,13 @@
+package hu.hobby.whattocook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToCookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatToCookApplication.class, args);
+    }
+
+}
